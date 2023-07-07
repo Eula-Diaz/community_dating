9b22b844cce3da2d119c00a5fb6970575a9d2263
muzi <xiumuds@163.com>
2023-07-07 23:17:18 +0800

    init

---
+++ pages/index/index.vue
@@ -0,0 +1,26 @@
+<template>
+	<view>
+		<!-- <text class="iconfont icon-bell" style="font-size: 100rpx;"></text> -->
+		<view class="animate__animated" style="border: 1rpx solid #333;padding: 20rpx;" hover-class="animate__pulse">点击效果</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	
+</style>
